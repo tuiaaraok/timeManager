954f67d555d9de3baa6dd71e85f1370ef6e03e59
Tuiaara <tuyara_ok@mail.ru>
2020-08-12 18:05:17 +0300

    Initial Commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by Айсен Шишигин on 12/08/2020.
+//  Copyright © 2020 Туйаара Оконешникова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
